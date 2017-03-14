94533be1e505c511b8358c884550ba5f8bb78325
Nicholas J Meyer <nick.j.meyer@gmail.com>
2017-03-14 12:08:26 -0400

    adapt test

--- njm_cpp/test/thread/test_pool.cpp
+++ njm_cpp/test/thread/test_pool.cpp
@@ -3,7 +3,7 @@
 #include <cstdint>
 #include <thread>
 #include <njm_cpp/thread/pool.hpp>
-#include <njm_cpp/data/result.hpp>
+#include <future>
 
 namespace njm {
 namespace thread {
@@ -16,22 +16,22 @@ TEST(TestPool, Counting) {
 
     const int32_t N = 1000 * num_threads;
 
-    std::vector<std::shared_ptr<data::Result<uint32_t> > > results;
+    std::vector<std::future<uint32_t> > results;
     for (uint32_t i = 0; i < N; ++i) {
-        const std::shared_ptr<data::Result<uint32_t> > ptr(
-                new data::Result<uint32_t>);
-        results.push_back(ptr);
-        p.service().post(std::bind(
-                        [](const std::shared_ptr<data::Result<uint32_t> > & r,
-                                const uint32_t & i) {
-                            r->set(i);
-                        }, ptr, i));
+        const std::shared_ptr<std::promise<uint32_t> > ptr(
+                new std::promise<uint32_t>);
+
+        results.push_back(ptr->get_future());
+
+        p.service().post([=]() {
+            ptr->set_value(i);
+        });
     }
 
     p.join();
 
     for (uint32_t i = 0; i < N; ++i) {
-        EXPECT_EQ(results.at(i)->get(), i);
+        EXPECT_EQ(results.at(i).get(), i);
     }
 }
 
