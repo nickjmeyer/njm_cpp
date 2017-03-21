30e82edcc797270e392175afa118c54fa08a0ab1
Nicholas J Meyer <nick.j.meyer@gmail.com>
2017-03-21 17:11:42 -0400

    continue implementation of b splines

--- njm_cpp/include/njm_cpp/fn_approx/bsplines.hpp
+++ njm_cpp/include/njm_cpp/fn_approx/bsplines.hpp
@@ -1,6 +1,104 @@
 #ifndef NJM_CPP_B_SPLINES_HPP
 #define NJM_CPP_B_SPLINES_HPP
 
+#include <cstdint>
+#include <vector>
+
+
+namespace njm {
+namespace fn_approx {
+
+
+class Bspline {
+protected:
+    const uint32_t degree_;
+    const uint32_t num_knots_;
+    const std::vector<double> knots_;
+
+    /**
+     * Evaluate one of the basis functions
+     *
+     * @param x value to evaluate at
+     *
+     * @param i index of basis function
+     *
+     * @param k degree of basis function
+     */
+    double eval_one_basis(const double & x, const double & i,
+            const double & k);
+
+    /**
+     * Evaluate the derivative to a specified degree
+     *
+     * @param x value to evaluate at
+     *
+     * @param deriv_degree degree of the derivative to evaluate
+     */
+    double deriv(const double & x, const uint32_t & deriv_degree,
+            const uint32_t & i, const uint32_t & k);
+
+    /**
+     * Helper function for computing integration
+     *
+     * @param x value to evaluate at
+     *
+     * @param i basis function index
+     *
+     * @param k degree of the basis function
+     */
+    double integ_helper(const double & x, const uint32_t & i,
+            const uint32_t & k);
+
+public:
+    Bspline(const uint32_t & degree,
+            const std::vector<double> & knots);
+
+    virtual ~Bspline() = default;
+
+    /**
+     * Evaluate the spline at value x and add basis functions
+     *
+     * @param x value to evaluate at
+     */
+    double eval(const double & x);
+
+    /**
+     * Evaluate the spline at value x and keep basis functions
+     * separate
+     *
+     * @param x value to evaluate at
+     */
+    std::vector<double> eval_basis(const double & x);
+
+    /**
+     * Evaluate the first derivative at x
+     *
+     * @param x value to evaluate at
+     */
+    double deriv(const double & x);
+
+    /**
+     * Evaluate the derivative to a specified degree
+     *
+     * @param x value to evaluate at
+     *
+     * @param deriv_degree degree of the derivative to evaluate
+     */
+    double deriv(const double & x, const uint32_t & deriv_degree);
+
+    /**
+     * Integrate over a range
+     *
+     * @param x_min lower integration bound
+     *
+     * @param x_max upper integration bound
+     */
+    double integ(const double & x_min, const double & x_max);
+};
+
+
+} // namespace fn_approx
+} // namespace njm
 
 
 #endif // NJM_CPP_B_SPLINES_HPP
--- njm_cpp/src/fn_approx/bsplines.cpp
+++ njm_cpp/src/fn_approx/bsplines.cpp
@@ -1,1 +1,129 @@
 #include <njm_cpp/fn_approx/bsplines.hpp>
+
+#include <glog/logging.h>
+
+
+namespace njm {
+namespace fn_approx {
+
+Bspline::Bspline(const uint32_t & degree,
+        const std::vector<double> & knots)
+    : degree_(degree), num_knots_(knots.size()), knots_(knots) {
+}
+
+
+double Bspline::eval_one_basis(const double & x, const double & i,
+        const double & k) {
+    if (i < 0) {
+        return 0.0;
+    } else if (i + k + 1 >= this->num_knots_) {
+        return 0.0;
+    } else if (k == 0) {
+        if (this->knots_.at(i) <= x && x < this->knots_.at(i + 1)) {
+            return 1.0;
+        } else {
+            return 0.0;
+        }
+    } else {
+        // evaluate first term
+        const double first_numer = x - this->knots_.at(i);
+        const double first_denom = this->knots_.at(i + k) - this->knots_.at(i);
+        double first = this->eval_one_basis(x, i, k - 1);
+
+        if (first > 0.0) {
+            first *= first_numer / first_denom;
+        }
+
+        // evaluate second term
+        const double second_numer = this->knots_.at(i + k + 1) - x;
+        const double second_denom = this->knots_.at(i + k + 1)
+            - this->knots_.at(i + 1);
+        double second = this->eval_one_basis(x, i + 1, k - 1);
+
+        if (second > 0.0) {
+            second *= second_numer / second_denom;
+        }
+
+        return first + second;
+    }
+}
+
+
+std::vector<double> Bspline::eval_basis(const double & x) {
+    const uint32_t num_fns = this->num_knots_ - this->degree_ - 1;
+
+    std::vector<double> basis;
+    basis.reserve(num_fns);
+
+    for (uint32_t i = 0; i < num_fns; ++i) {
+        basis.push_back(this->eval_one_basis(x, i, this->degree_));
+    }
+
+    return basis;
+}
+
+
+double Bspline::deriv(const double & x, const uint32_t & deriv_degree,
+        const uint32_t & i, const uint32_t & k) {
+    if (deriv_degree == 0) {
+        return this->eval_one_basis(x, i, k);
+    } else if (k < 2) {
+        return 0.0;
+    } else {
+        const double first_denom = this->knots_.at(i + k)
+            - this->knots_.at(i + 1);
+        const double first = this->deriv(x, deriv_degree - 1,
+                i + 1, k - 1);
+
+        const double second_denom = this->knots_.at(i + k - 1)
+            - this->knots_.at(i);
+        const double second = this->deriv(x, deriv_degree - 1,
+                i, k - 1);
+
+        return (k - 1) * (first / first_denom + second / second_denom);
+    }
+}
+
+
+double Bspline::deriv(const double & x) {
+    return this->deriv(x, 1);
+}
+
+
+double Bspline::deriv(const double & x, const uint32_t & deriv_degree) {
+    CHECK_GT(deriv_degree, 0)
+        << "For derivative degree 0, just use Bspline::eval";
+
+    const uint32_t num_fns = this->num_knots_ - this->degree_ - 1;
+
+    double value = 0.0;
+    for (uint32_t i = 0; i < num_fns; ++i) {
+        value += this->deriv(x, deriv_degree, i, this->degree_);
+    }
+
+    return value;
+}
+
+double Bspline::integ(const double & x, const uint32_t & i,
+        const uint32_t & k) {
+    const double knot_a = this->knot_.at(i + k + 1);
+    // TODO: finish integration function
+}
+
+
+double Bspline::integ(const double & x_min, const double & x_max) {
+    const uint32_t num_fns = this->num_knots_ - this->degree_ - 1;
+
+    double value = 0.0;
+    for (uint32_t i = 0; i < num_fns; ++i) {
+        value += this->integ_helper(x_max, i, this->degree - 1);
+        value -= this->integ_helper(x_min, i, this->degree - 1);
+
+        value += this->integ_helper(x_max, i + 1, this->degree - 1);
+        value -= this->integ_helper(x_min, i + 1, this->degree - 1);
+    }
+}
+
+
+} // namespace fn_approx
+} // namespace njm
