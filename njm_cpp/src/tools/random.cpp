4579ab8402434882b9b42a3b55492a6951f7efbe
Nicholas J Meyer <nick.j.meyer@gmail.com>
2017-04-14 13:32:08 -0400

    remove printing

--- njm_cpp/src/tools/random.cpp
+++ njm_cpp/src/tools/random.cpp
@@ -12,11 +12,9 @@ Rng::Rng()
     : gen_(0), seed_(0), dis_runif_01_(0., 1.),
       has_next_rnorm_(false), next_rnorm_01_(0.0),
       runif_count_(0), rnorm_count_(0) {
-    std::cout << "CALL: " << __FUNCTION__ << " (" << this << ")" << std::endl;
 }
 
 void Rng::seed(const uint32_t seed) {
-    std::cout << "CALL: " << __FUNCTION__ << " (" << this << ")" << std::endl;
     std::lock_guard<std::mutex> lock(this->gen_mutex_);
     this->seed_ = seed;
     this->gen_.seed(seed);
@@ -26,46 +24,33 @@ void Rng::seed(const uint32_t seed) {
 
 
 uint32_t Rng::seed() const {
-    std::cout << "CALL: " << __FUNCTION__ << " (" << this << ")" << std::endl;
     std::lock_guard<std::mutex> lock(this->gen_mutex_);
     return this->seed_;
 }
 
 const std::mt19937 & Rng::gen() const {
-    std::cout << "CALL: " << __FUNCTION__ << " (" << this << ")" << std::endl;
     std::lock_guard<std::mutex> lock(this->gen_mutex_);
     return this->gen_;
 }
 
 void Rng::gen(const std::mt19937 & gen) {
-    std::cout << "CALL: " << __FUNCTION__ << " (" << this << ")" << std::endl;
     std::lock_guard<std::mutex> lock(this->gen_mutex_);
     this->gen_ = gen;
 }
 
 double Rng::runif_01() {
-    std::cout << "CALL: " << __FUNCTION__ << " (" << this << ")" << std::endl;
     std::lock_guard<std::mutex> lock(this->gen_mutex_);
     ++this->runif_count_;
     const double draw(this->dis_runif_01_(this->gen_));
-    std::cout << "runif count: " << this->runif_count_
-              << "  (" << draw
-              << ", " << this->seed_
-              << ", " << this << ")" << std::endl;
     return draw;
 }
 
 
 double Rng::rnorm_01() {
-    std::cout << "CALL: " << __FUNCTION__ << " (" << this << ")" << std::endl;
     std::lock_guard<std::mutex> lock(this->gen_mutex_);
     ++this->rnorm_count_;
-    std::cout << "rnorm count: " << this->rnorm_count_;
     if (this->has_next_rnorm_) {
         this->has_next_rnorm_ = false;
-        std::cout << "  (" << this->next_rnorm_01_
-                  << ", " << this->seed_
-                  << ", " << this << ")" << std::endl;
         return this->next_rnorm_01_;
     } else {
         const double u1(this->dis_runif_01_(this->gen_));
@@ -74,34 +59,27 @@ double Rng::rnorm_01() {
         this->has_next_rnorm_ = true;
         this->next_rnorm_01_ = a * std::cos(6.28318530718 * u2);
         const double draw(a * std::sin(6.28318530718 * u2));
-        std::cout << "  (" << draw
-                  << ", " << this->seed_
-                  << ", " << this << ")" << std::endl;
         return draw;
     }
 }
 
 double Rng::rnorm(const double mu, const double sigma) {
-    std::cout << "CALL: " << __FUNCTION__ << " (" << this << ")" << std::endl;
     return this->rnorm_01() * sigma + mu;
 }
 
 double Rng::runif(const double a, const double b) {
-    std::cout << "CALL: " << __FUNCTION__ << " (" << this << ")" << std::endl;
     CHECK_LT(a, b);
     return this->runif_01() * (b - a) + a;
 }
 
 
 int Rng::rint(const int a, const int b) {
-    std::cout << "CALL: " << __FUNCTION__ << " (" << this << ")" << std::endl;
     CHECK_LT(a, b);
     return static_cast<int>(this->runif_01() * (b - a)) + a;
 }
 
 
 std::vector<int> Rng::sample_range(const int a, const int b, const int n) {
-    std::cout << "CALL: " << __FUNCTION__ << " (" << this << ")" << std::endl;
     CHECK_LT(a, b) << "a = " << a << ", b = " << b;
     const uint32_t num_vals = b - a;
     CHECK_LT(n, num_vals); // can't sample more than what's there
@@ -126,7 +104,6 @@ std::vector<int> Rng::sample_range(const int a, const int b, const int n) {
 }
 
 void Rng::shuffle(std::vector<uint32_t> & x) {
-    std::cout << "CALL: " << __FUNCTION__ << " (" << this << ")" << std::endl;
     const uint32_t x_size(x.size());
     for (uint32_t i = 0; i < (x_size - 1); ++i) {
         const uint32_t draw_index(static_cast<uint32_t>(this->rint(i, x_size)));
@@ -141,32 +118,26 @@ void Rng::shuffle(std::vector<uint32_t> & x) {
 
 RngClass::RngClass()
     : rng_(new Rng()) {
-    std::cout << "CALL: " << __FUNCTION__ << " (" << this << ")" << std::endl;
 }
 
 RngClass::RngClass(const RngClass & other)
     : RngClass() {
-    std::cout << "CALL: " << __FUNCTION__ << " (" << this << ")" << std::endl;
     this->seed(other.seed());
 }
 
 void RngClass::rng(const std::shared_ptr<Rng> & rng) {
-    std::cout << "CALL: " << __FUNCTION__ << " (" << this << ")" << std::endl;
     this->rng_ = rng;
 }
 
 const std::shared_ptr<Rng> & RngClass::rng() const {
-    std::cout << "CALL: " << __FUNCTION__ << " (" << this << ")" << std::endl;
     return this->rng_;
 }
 
 void RngClass::seed(const uint32_t seed) {
-    std::cout << "CALL: " << __FUNCTION__ << " (" << this << ")" << std::endl;
     this->rng_->seed(seed);
 }
 
 uint32_t RngClass::seed() const {
-    std::cout << "CALL: " << __FUNCTION__ << " (" << this << ")" << std::endl;
     return this->rng_->seed();
 }
 
